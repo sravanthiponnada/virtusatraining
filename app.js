f5a120cd5580a790bb5256034ff7d1ccb6eb348e
sravanthiponnada <43266512+sravanthiponnada@users.noreply.github.com>
2018-09-25 14:32:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,4 +37,12 @@ function changeBackColor(color1,id)
 {
     document.getElementsByClassName(id).style.backgroundColor="green";
 }
+function numbersOnly(event){
+    event = (event) ? event : window.event;
+    var charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
 
+}
